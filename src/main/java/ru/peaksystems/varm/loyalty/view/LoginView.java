658bbe6014f5779bc0b37b9e4cd2f7160693b323
croacker <7RV5699400>
2015-08-25 17:57:45 +0300

    I-7810 Новая версия ЛК Сотрудника коллцентра

--- src/main/java/ru/peaksystems/varm/loyalty/view/LoginView.java
+++ src/main/java/ru/peaksystems/varm/loyalty/view/LoginView.java
@@ -4,7 +4,9 @@ import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Page;
 import com.vaadin.server.Responsive;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.Position;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 import ru.ml.core.common.guice.GuiceConfigSingleton;
@@ -28,8 +30,72 @@ public class LoginView extends VerticalLayout {
 
     public LoginView() {
         setSizeFull();
+
+        HorizontalLayout headerLayout = new HorizontalLayout();
+        headerLayout.setWidth("100%");
+
+        Image msrPic = new Image(null, new ThemeResource("img/msc_logo.png"));
+        msrPic.setWidth(200, Unit.PIXELS);
+        headerLayout.addComponent(msrPic);
+        headerLayout.setComponentAlignment(msrPic, Alignment.MIDDLE_LEFT);
+
+        VerticalLayout contactsLayout = new VerticalLayout();
+        Label phoneLabel = new Label("+7 (495) 539 55 55");
+        contactsLayout.addComponent(phoneLabel);
+        Label noteLabel = new Label("Горячая линия");
+        contactsLayout.addComponent(noteLabel);
+        contactsLayout.setSizeUndefined();
+
+        headerLayout.addComponent(contactsLayout);
+        headerLayout.setComponentAlignment(contactsLayout, Alignment.MIDDLE_RIGHT);
+
+        addComponent(headerLayout);
+        addComponent(buildSparklines());
+
+        ///
         Component loginForm = buildLoginForm();
         addComponent(loginForm);
+        setExpandRatio(loginForm, 1);
+        //
+
+        HorizontalLayout footerLayout = new HorizontalLayout();
+        footerLayout.setWidth("100%");
+
+        String footerContent = " <div id=\"template_footer_top\" class=\"template_area\"> "
+        + " <div class=\"by_left\"> "
+        + " <div id=\"copyright\">©&nbsp;2015&nbsp;ГУП&nbsp;МСР</div> "
+                + " <address class=\"contacts\"> "
+                + " <div class=\"address\">г. Москва, Серпуховской переулок, д. 7, стр. 1</div> "
+                + " </address> "
+                + " </div> "
+                + " <div class=\"by_right\"> "
+                + " <address class=\"contacts\"> "
+                + " <div class=\"phone\">+7 (495) <strong>539 55 55</strong></div> "
+                + " <div class=\"note\">Горячая линия</div> "
+                + " </address> "
+                + " <div id=\"worktime\">Время работы: 08:00 - 20:00 Пн-Вс</div> "
+                + " </div> "
+                + " </div> "
+                + " <div id=\"template_footer_bottom\"> "
+                + " <nav id=\"bottom_nav_menu\" class=\"template_area\"> "
+                + " <ul class=\"nav_menu\"> "
+                + " <li class=\"nav_menu_item\"><a class=\"nav_menu_item_link\" target=\"_blank\" href=\"http://www.soccard.ru/\">О "
+                + " проекте</a></li> "
+                + " <li class=\"nav_menu_item\"><a class=\"nav_menu_item_link\" target=\"_blank\" href=\"http://www.soccard.ru/\">О "
+                + " предприятии</a></li> "
+                + " <li class=\"nav_menu_item\"><a class=\"nav_menu_item_link\" target=\"_blank\" href=\"http://www.soccard.ru/\">Пресс-центр</a> "
+                + " </li> "
+                + " <li class=\"nav_menu_item\"><a class=\"nav_menu_item_link\" target=\"_blank\" href=\"http://www.soccard.ru/\">Стать "
+                + " партнером</a></li> "
+                + " </ul> "
+                + " </nav> "
+                + " </div> ";
+        Label footerLabel = new Label(footerContent, ContentMode.HTML);
+        footerLabel.setSizeFull();
+        footerLayout.addComponent(footerLabel);
+
+        addComponent(footerLayout);
+
         setComponentAlignment(loginForm, Alignment.MIDDLE_CENTER);
     }
 
@@ -123,5 +189,14 @@ public class LoginView extends VerticalLayout {
 
     private void showError(String text){
         Notification.show(text, Notification.Type.ERROR_MESSAGE);
+    }
+
+    private Component buildSparklines() {
+        CssLayout sparks = new CssLayout();
+        sparks.addStyleName("sparks");
+        sparks.setWidth("100%");
+        Responsive.makeResponsive(sparks);
+
+        return sparks;
     }
 }
